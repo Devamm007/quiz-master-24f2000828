49fa5aafc45ee5903deafc584830fcf8767abef6
24f2000828 <24f2000828@ds.study.iitm.ac.in>
2025-02-06 17:03:22 +0000

    Implemented logout and password change feature
    Created a nav bar in boilerplate.html, with a profile html to change username/password

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, redirect, url_for, flash, session
 from bcrypt import hashpw, gensalt, checkpw
+from functools import wraps
 
 app = Flask(__name__)
 
@@ -17,9 +18,20 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = os.getenv('SQLALCHEMY_TRACK_MODIF
 from models.models import db, Registrations, Subjects, Chapters, Quizzes, Questions, UserInput, Scores
 
 # ROUTES: routes.py
-@app.route('/login')
+
+def authentication(func): # decorator for authentication
+    @wraps(func)
+    def auth(*args, **kwargs):
+        if 'user_id' in session:
+            return func(*args, **kwargs)
+        else:
+            flash("Please login to continue", "error")
+            return redirect(url_for("login"))
+    return auth
+
+@app.route("/login")
 def login():
-    return render_template("login.html")
+    return render_template("login.html", user=None)
 
 @app.route("/login", methods=["POST"])
 def login_post():
@@ -38,14 +50,21 @@ def login_post():
     
     session["user_id"] = user.id
     if user.is_admin:
-        flash("Welcome admin!", "error")
+        flash("Welcome admin!", "success")
+        return redirect(url_for("admin"))
     else:
-        flash("Login succesful!", "error")
+        flash("Login successful!", "success")
     return redirect(url_for("home"))
+
+@app.route("/logout")
+def logout():
+    session.pop("user_id")
+    flash("You have logged out.", "success")
+    return redirect(url_for("login"))
 
 @app.route("/registration")
 def registration():
-    return render_template("register.html")
+    return render_template("register.html", user=None)
 
 @app.route("/registration", methods=["POST"])
 def registration_post():
@@ -80,6 +99,52 @@ def registration_post():
     db.session.add(new_user) #no need to worry about starting/ending the session, as we are using ORM of SQLAlchemy and not SQL Query
     db.session.commit()
     return redirect(url_for("login"))
+
+@app.route("/home")
+@authentication
+def home():
+    user = Registrations.query.get(session['user_id'])
+    if user.is_admin:
+        return redirect(url_for("admin", user=user))
+    return render_template("home.html", user=user)
+
+@app.route("/profile")
+@authentication
+def profile():
+    return render_template("profile.html", user=Registrations.query.get(session['user_id']))
+
+@app.route("/profile", methods=["POST"])
+@authentication
+def profile_post():
+    username = request.form.get("username")
+    curr_password = request.form.get("curr_password")
+    password = request.form.get("password")
+
+    if not username or not curr_password or not password:
+        flash("Please fill all the fields", "error")
+        return redirect(url_for("profile"))
+    
+    user = Registrations.query.get(session['user_id'])
+    if not checkpw(curr_password.encode("utf-8"), user.passhash):
+        flash("Incorrect password!", "error")
+        return redirect(url_for("profile"))
+    
+    if username != user.username:
+        new_username = Registrations.query.filter_by(username=username).first()
+        if new_username:
+            flash("Username already exists!", "error")
+            return redirect(url_for("profile"))
+        
+    salt = gensalt()
+    new_passhash = hashpw(password.encode("utf-8"), salt)
+    user.username = username
+    user.passhash = new_passhash
+    db.session.commit()
+    return redirect(url_for("profile"))
+
+@app.route("/admin")
+def admin():
+    return render_template("admin.html", user=Registrations.query.get(session['user_id']))
     
 if  __name__ == "__main__":
     app.run(debug=True)
